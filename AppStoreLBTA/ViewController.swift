d87304d6ddd299863423fe71d917ef07dfb6d684
Lasse N Silkoset <lassesilkoset@gmail.com>
2019-02-28 15:13:42 +0100

    Initial Commit

---
+++ AppStoreLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreLBTA
+//
+//  Created by Lasse Silkoset on 28/02/2019.
+//  Copyright © 2019 Lasse Silkoset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
